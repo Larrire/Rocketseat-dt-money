ef80b8497d77cf3d8face542c0929525a51eef39
Larrire <linekerlarrire@gmail.com>
2022-05-16 09:06:58 -0300

    Movendo o modal do Header para o App e repassando função de fechar modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,17 @@
-import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
-import Modal from 'react-modal';
 
-export function Header () {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
 
-  function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true)
-  }
-
-  function handleCloseNewTransactionModal() {
-    setIsNewTransactionModalOpen(false)
-  }
-
+export function Header ({onOpenNewTransactionModal}: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money"/>
-        <button type="button" onClick={handleOpenNewTransactionModal}>Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>Nova transação</button>
       </Content>
-
-      <Modal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
     </Container>
   )
 }
